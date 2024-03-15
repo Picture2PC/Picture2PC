bc6cf696ec5b81ab6c6256ad14bdc40689d50ed1
GrumpyDucc <j.waldinger@online.de>
2024-03-15 18:05:36 +0100

    ANDROID | Updated app icon

--- android/src/main/kotlin/com/github/picture2pc/android/ui/main/mainscreen/elements/BrandingTopBar.kt
+++ android/src/main/kotlin/com/github/picture2pc/android/ui/main/mainscreen/elements/BrandingTopBar.kt
@@ -1,23 +1,28 @@
 package com.github.picture2pc.android.ui.main.mainscreen.elements
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-//import com.github.picture2pc.common.ui.Picture2PcIcon
 import com.github.picture2pc.android.R
 
 @Composable
 fun BrandingTopBar(modifier: Modifier) {
     Row(
-        verticalAlignment = Alignment.CenterVertically, modifier = modifier
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
     ) {
         Text(
             fontSize = 24.sp,
@@ -25,12 +30,14 @@ fun BrandingTopBar(modifier: Modifier) {
             textAlign = TextAlign.Center,
             modifier = Modifier.fillMaxWidth(0.6F)
         )
-        Image(
-            painter = painterResource(id = R.drawable.icon),
-            contentDescription = "ICON",
-            modifier = Modifier
-                .fillMaxWidth()
-                .scale(0.85F)
-        )
+       Image(
+           painter = painterResource(id = R.drawable.icon),
+           contentDescription = "ICON",
+           modifier = Modifier
+               .fillMaxWidth()
+               .scale(.8F)
+               .background(Color.White, RoundedCornerShape(20.dp))
+               .padding(8.dp)
+       )
     }
 }
