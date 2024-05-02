418800e6b3d747cfd26596f59d12eb947b185cc3
GrumpyDucc <j.waldinger@online.de>
2024-05-02 22:14:51 +0200

    ANDROID | repositioned image

--- android/src/main/kotlin/com/github/picture2pc/android/ui/main/camerascreen/CameraScreen.kt
+++ android/src/main/kotlin/com/github/picture2pc/android/ui/main/camerascreen/CameraScreen.kt
@@ -1,25 +1,12 @@
 package com.github.picture2pc.android.ui.main.camerascreen
 
-import android.util.Log
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.unit.dp
 import com.github.picture2pc.android.ui.main.camerascreen.elements.CameraPreview
 import com.github.picture2pc.android.ui.main.camerascreen.elements.DisplayImage
 import com.github.picture2pc.android.ui.main.camerascreen.elements.PictureButtons
