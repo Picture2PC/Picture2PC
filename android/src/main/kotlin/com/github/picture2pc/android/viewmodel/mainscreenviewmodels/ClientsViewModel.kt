bd525e2a96bb64ba19e09a2a516da37d9f1db7ef
mightytry <maik@rostocki.net>
2024-05-04 17:57:00 +0200

    ANDROID | make connectedPeers not mutable outside tcp

--- android/src/main/kotlin/com/github/picture2pc/android/viewmodel/mainscreenviewmodels/ClientsViewModel.kt
+++ android/src/main/kotlin/com/github/picture2pc/android/viewmodel/mainscreenviewmodels/ClientsViewModel.kt
@@ -1,19 +1,12 @@
 package com.github.picture2pc.android.viewmodel.mainscreenviewmodels
 
-import com.github.picture2pc.common.net2.Peer
 import com.github.picture2pc.common.net2.impl.tcp.TcpPayloadTransceiver
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlin.coroutines.CoroutineContext
 
 class ClientsViewModel(
     tcpPayloadTransceiver: TcpPayloadTransceiver,
     override val coroutineContext: CoroutineContext
 ) : CoroutineScope {
-    var serverEntries = MutableStateFlow<List<Peer>>(emptyList()).asStateFlow()
-
-    init {
-        serverEntries = tcpPayloadTransceiver.connectedPeers.asStateFlow()
-    }
+    val serverEntries = tcpPayloadTransceiver.connectedPeers
 }
