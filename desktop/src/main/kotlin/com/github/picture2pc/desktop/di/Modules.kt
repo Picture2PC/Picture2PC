bc26520f1c6e33ac14e2fed4934f9469f35b6127
GrumpyDucc <j.waldinger@online.de>
2024-08-03 18:06:06 +0200

    DESKTOP | First recieved image shows up automatically

--- desktop/src/main/kotlin/com/github/picture2pc/desktop/di/Modules.kt
+++ desktop/src/main/kotlin/com/github/picture2pc/desktop/di/Modules.kt
@@ -6,6 +6,7 @@ import com.github.picture2pc.desktop.data.availableserverscollector.impl.Multica
 import com.github.picture2pc.desktop.net.datatransmitter.DataReceiver
 import com.github.picture2pc.desktop.net.datatransmitter.impl.TcpPictureReceiver
 import com.github.picture2pc.desktop.viewmodel.picturedisplayviewmodel.PictureDisplayViewModel
+import com.github.picture2pc.desktop.viewmodel.pictureeditorviewmodel.PictureEditorViewModel
 import com.github.picture2pc.desktop.viewmodel.serversectionviewmodel.ServersSectionViewModel
 import org.koin.dsl.module
 
@@ -13,7 +14,9 @@ val appModule = module {
     includes(commonAppModule)
 
     single<AvailableServersCollector> { MulticastAvailableServersCollector(get(), get(), get()) }
-    single { ServersSectionViewModel(get(), get()) }
     single<DataReceiver> { TcpPictureReceiver(get(), get()) }
-    single { PictureDisplayViewModel(get()) }
+
+    single { ServersSectionViewModel(get(), get()) }
+    single { PictureDisplayViewModel(get(), get()) }
+    single { PictureEditorViewModel(get()) }
 }
--- desktop/src/main/kotlin/com/github/picture2pc/desktop/viewmodel/picturedisplayviewmodel/PictureDisplayViewModel.kt
+++ desktop/src/main/kotlin/com/github/picture2pc/desktop/viewmodel/picturedisplayviewmodel/PictureDisplayViewModel.kt
@@ -1,8 +1,10 @@
 package com.github.picture2pc.desktop.viewmodel.picturedisplayviewmodel
 
 import com.github.picture2pc.desktop.net.datatransmitter.DataReceiver
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import org.jetbrains.skia.Image
 import java.awt.Toolkit
 import java.awt.datatransfer.Clipboard
@@ -11,36 +13,30 @@ import java.awt.datatransfer.Transferable
 import java.awt.datatransfer.UnsupportedFlavorException
 import java.awt.image.BufferedImage
 import javax.imageio.ImageIO
+import kotlin.coroutines.CoroutineContext
 
 class PictureDisplayViewModel(
-    dataReceiver: DataReceiver
-) {
+    dataReceiver: DataReceiver,
+    override val coroutineContext: CoroutineContext
+) : CoroutineScope {
     private val pictures = dataReceiver.pictures
     private var currentPictureIndex : Int = 0
 
     private val _currentPicture = MutableStateFlow<Image?>(null)
     val currentPicture: StateFlow<Image?> = _currentPicture
 
+    init {
+        CoroutineScope(coroutineContext).launch {
+            pictures.collect { pictureList ->
+                if (!pictureList.isEmpty && _currentPicture.value == null) {
+                    updateCurrentPicture()
+                }
+            }
+        }
+    }
+
     private fun updateCurrentPicture() {
         _currentPicture.value = pictures.replayCache[currentPictureIndex]
-    }
-
-    fun adjustCurrentPictureIndex(increase:Boolean) {
-        if (pictures.replayCache.isEmpty()) return
-        if (increase && currentPictureIndex < pictures.replayCache.size - 1) {
-            currentPictureIndex++
-        } else if (!increase && currentPictureIndex > 0) {
-            currentPictureIndex--
-        }
-        updateCurrentPicture()
-    }
-
-    fun addPictureToClipboard() {
-        if (currentPicture.value == null) return
-        val bufferedImage = currentPicture.value?.toBufferedImage()
-        val transferableImage = bufferedImage?.let { TransferableImage(it) }
-        val clipboard: Clipboard = Toolkit.getDefaultToolkit().systemClipboard
-        clipboard.setContents(transferableImage, null)
     }
 
     private fun Image.toBufferedImage(): BufferedImage {
@@ -64,6 +60,28 @@ class PictureDisplayViewModel(
         override fun isDataFlavorSupported(flavor: DataFlavor?): Boolean {
             return flavor == DataFlavor.imageFlavor
         }
+    }
+
+    fun adjustCurrentPictureIndex(increase:Boolean) {
+        if (pictures.replayCache.isEmpty()) return
+        if (increase && currentPictureIndex < pictures.replayCache.size - 1) {
+            currentPictureIndex++
+        } else if (!increase && currentPictureIndex > 0) {
+            currentPictureIndex--
+        }
+        updateCurrentPicture()
+    }
+
+    fun addPictureToClipboard() {
+        if (currentPicture.value == null) return
+        val bufferedImage = currentPicture.value?.toBufferedImage()
+        val transferableImage = bufferedImage?.let { TransferableImage(it) }
+        val clipboard: Clipboard = Toolkit.getDefaultToolkit().systemClipboard
+        clipboard.setContents(transferableImage, null)
+    }
+
+    fun getPictureDimensions(): Pair<Int, Int> {
+        return Pair(currentPicture.value?.width ?: 0, currentPicture.value?.height ?: 0)
     }
 
     fun testAction() {
