fe1abcd5dc426bd2385ce5567388c0f21ae51818
GrumpyDucc <j.waldinger@online.de>
2024-08-27 18:13:02 +0200

    ANDROID | Click on screen does not show ripple animation

--- android/src/main/kotlin/com/github/picture2pc/android/ui/main/Screen.kt
+++ android/src/main/kotlin/com/github/picture2pc/android/ui/main/Screen.kt
@@ -3,11 +3,13 @@ package com.github.picture2pc.android.ui.main
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
@@ -30,7 +32,11 @@ fun Screen(vertical: Boolean, screenSelector: ScreenSelectorViewModel = remember
                 .fillMaxSize()
                 .padding(10.dp)
                 .border(4.dp, Style.Colors.PRIMARY, RoundedCornerShape(25.dp))
-                .clickable(onClick = focusManager::clearFocus)
+                .clickable(
+                    onClick = focusManager::clearFocus,
+                    interactionSource = remember { MutableInteractionSource() },
+                    indication = null
+                )
         ) {
             when (screenSelector.value) {
                 ScreenSelectorViewModel.Screens.MAIN -> MainScreen()
