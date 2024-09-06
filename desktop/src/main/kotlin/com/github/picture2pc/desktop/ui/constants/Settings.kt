0f998119e245308a4999c933415f0f36b0848e33
GrumpyDucc <j.waldinger@online.de>
2024-09-06 19:32:04 +0200

    DESKTOP | Fixup

--- desktop/src/main/kotlin/com/github/picture2pc/desktop/ui/constants/Settings.kt
+++ desktop/src/main/kotlin/com/github/picture2pc/desktop/ui/constants/Settings.kt
@@ -3,4 +3,6 @@ package com.github.picture2pc.desktop.ui.constants
 object Settings {
     const val SCALE = 1.5f
     const val ZOOM_DIAMETER = 140
+    const val HIGH_DRAGGING_SPEED = 1.0f
+    const val SLOW_DRAGGING_SPEED = 0.5f
 }
