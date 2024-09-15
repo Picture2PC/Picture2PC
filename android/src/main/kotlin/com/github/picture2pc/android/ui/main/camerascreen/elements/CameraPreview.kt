e9fe29ea69c863c40d52a69948c940a6768b6b35
GrumpyDucc <j.waldinger@online.de>
2024-09-15 21:21:11 +0200

    ANDROID | fixed picture turned 90° in preview

--- android/src/main/kotlin/com/github/picture2pc/android/ui/main/camerascreen/elements/CameraPreview.kt
+++ android/src/main/kotlin/com/github/picture2pc/android/ui/main/camerascreen/elements/CameraPreview.kt
@@ -2,22 +2,14 @@ package com.github.picture2pc.android.ui.main.camerascreen.elements
 
 import androidx.camera.view.PreviewView
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.viewinterop.AndroidView
 import com.github.picture2pc.android.viewmodel.camerascreenviewmodels.CameraViewModel
-import com.github.picture2pc.common.ui.Shapes
 
 @Composable
 fun CameraPreview(
     cameraViewModel: CameraViewModel
 ) {
     AndroidView(
-        modifier = Modifier.clip(Shapes.MOBILE),
-        factory = {
-            PreviewView(it).apply {
-                cameraViewModel.setViewFinder(this)
-            }
-        }
+        factory = { PreviewView(it).apply { cameraViewModel.setViewFinder(this) } }
     )
 }
