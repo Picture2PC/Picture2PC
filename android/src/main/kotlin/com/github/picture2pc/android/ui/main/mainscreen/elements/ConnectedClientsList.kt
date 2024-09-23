1ccad46db18c77abe1049b121671078052821f59
GrumpyDucc <j.waldinger@online.de>
2024-09-23 13:45:25 +0200

    ANDROID | Added State to mobile connection

--- android/src/main/kotlin/com/github/picture2pc/android/ui/main/mainscreen/elements/ConnectedClientsList.kt
+++ android/src/main/kotlin/com/github/picture2pc/android/ui/main/mainscreen/elements/ConnectedClientsList.kt
@@ -1,24 +1,31 @@
 package com.github.picture2pc.android.ui.main.mainscreen.elements
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.github.picture2pc.android.viewmodel.mainscreenviewmodels.ClientsViewModel
 import com.github.picture2pc.common.ui.Colors
+import com.github.picture2pc.common.ui.Spacers
 import com.github.picture2pc.common.ui.TextStyles
 import org.koin.compose.rememberKoinInject
 
@@ -37,7 +44,10 @@ fun ConnectedClientsList(
         Row {
             Text(
                 text = "Connections",
-                style = TextStyles.NORMAL.copy(fontSize = 22.sp, fontWeight = FontWeight.Bold)
+                style = TextStyles.NORMAL.copy(
+                    fontSize = 22.sp,
+                    fontWeight = FontWeight.Bold
+                )
             )
         }
         Spacer(modifier = Modifier.height(10.dp))
@@ -50,11 +60,28 @@ fun ConnectedClientsList(
             } else {
                 LazyColumn {
                     items(connections) { client ->
-                        val clientName = client.name.collectAsState()
-                        Text(
-                            text = clientName.value,
-                            style = TextStyles.NORMAL.copy(fontSize = 20.sp)
-                        )
+                        val clientName = client.name.collectAsState().value
+                        val clientState = client.deviceState.collectAsState().value
+                        Row {
+                            Text(
+                                text = clientName,
+                                style = TextStyles.NORMAL.copy(fontSize = 20.sp)
+                            )
+                            Spacer(Modifier.weight(1f))
+                            Text(
+                                text = clientState.displayName,
+                                modifier = Modifier.align(Alignment.CenterVertically),
+                                style = TextStyles.SMALL.copy(fontSize = 16.sp)
+                            )
+                            Spacer(Modifier.width(Spacers.SMALL))
+                            Box(
+                                Modifier
+                                    .size(10.dp)
+                                    .clip(CircleShape)
+                                    .background(clientState.color)
+                                    .align(Alignment.CenterVertically)
+                            )
+                        }
                     }
                 }
             }
