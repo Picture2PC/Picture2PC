683994fd46e76a539ca4b047222dbb65a952140f
GrumpyDucc <j.waldinger@online.de>
2024-09-27 20:19:15 +0200

    DESKTOP | added input text field

--- desktop/src/main/kotlin/com/github/picture2pc/desktop/ui/main/elements/Sidebar.kt
+++ desktop/src/main/kotlin/com/github/picture2pc/desktop/ui/main/elements/Sidebar.kt
@@ -11,6 +11,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -18,15 +22,19 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.github.picture2pc.common.ui.Colors
+import com.github.picture2pc.common.ui.Heights
 import com.github.picture2pc.common.ui.Icons
 import com.github.picture2pc.common.ui.Shapes
 import com.github.picture2pc.common.ui.Spacers
+import com.github.picture2pc.common.ui.TextStyles
 import com.github.picture2pc.desktop.ui.constants.Descriptions
 import com.github.picture2pc.desktop.ui.constants.Settings
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Sidebar() {
     val showConnections = remember { mutableStateOf(true) }
+    val clientName = remember { mutableStateOf("") }
 
     Box(
         Modifier
@@ -37,6 +45,26 @@ fun Sidebar() {
         // Items in the Sidebar
         Column(Modifier.padding(Spacers.NORMAL).fillMaxSize()) {
             Header()
+            Spacer(Modifier.height(Spacers.LARGE))
+
+            OutlinedTextField(
+                value = clientName.value,
+                onValueChange = { clientName.value = it },
+                label = { Text("Name") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(Heights.BUTTON + 10.dp),
+                singleLine = true,
+                shape = Shapes.BUTTON,
+                textStyle = TextStyles.NORMAL,
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = Colors.PRIMARY,
+                    unfocusedBorderColor = Colors.PRIMARY,
+                    cursorColor = Colors.ACCENT,
+                    focusedLabelColor = Colors.TEXT,
+                    unfocusedLabelColor = Colors.TEXT.copy(alpha = 0.8f),
+                ),
+            )
             Spacer(Modifier.height(Spacers.LARGE))
 
             Row { ImageInteractionButtons() }
