4ba06267e69154ddc7e9da1d3d4f6f88e9e11e13
GrumpyDucc <j.waldinger@online.de>
2024-10-27 17:02:04 +0100

    DESKTOP | fixup, cleanup

--- desktop/src/main/kotlin/com/github/picture2pc/desktop/ui/main/MainScreen.kt
+++ desktop/src/main/kotlin/com/github/picture2pc/desktop/ui/main/MainScreen.kt
@@ -3,32 +3,30 @@ package com.github.picture2pc.desktop.ui.main
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.unit.dp
 import com.github.picture2pc.common.ui.Borders
 import com.github.picture2pc.common.ui.Colors
 import com.github.picture2pc.common.ui.Shapes
 import com.github.picture2pc.common.ui.Spacers
+import com.github.picture2pc.desktop.ui.constants.Settings
 import com.github.picture2pc.desktop.ui.main.elements.Picture
 import com.github.picture2pc.desktop.ui.main.elements.RotationButtons
 import com.github.picture2pc.desktop.ui.main.elements.Sidebar
 import com.github.picture2pc.desktop.ui.main.elements.ZoomSpeedButton
-import com.github.picture2pc.desktop.viewmodel.mainscreen.MovementHandlerViewModel
-import org.koin.compose.rememberKoinInject
 
 
 @Composable
-fun MainScreen(
-    mDVM: MovementHandlerViewModel = rememberKoinInject()
-) {
+fun MainScreen() {
     Box(
         Modifier
             .fillMaxSize()
@@ -37,30 +35,31 @@ fun MainScreen(
         Row(
             Modifier.padding(10.dp).fillMaxSize()
         ) {
-            Sidebar()
+            Column(
+                Modifier
+                    .fillMaxHeight()
+                    .width(Settings.SIDEBAR_WIDTH.dp)
+                    .background(Colors.SECONDARY, Shapes.WINDOW)
+            ) { Sidebar() }
             Spacer(Modifier.width(Spacers.NORMAL))
 
             Box(Modifier.fillMaxSize()) {
                 Box(
-                    Modifier.border(
-                        Borders.BORDER_STANDARD,
-                        Colors.PRIMARY,
-                        Shapes.WINDOW
-                    )
-                ) {
-                    Box(
-                        Modifier
-                            .rotate(mDVM.rotationState.value.angle)
-                            .padding(Spacers.NORMAL)
-                            .fillMaxSize(),
-                        Alignment.Center
-                    ) { Picture() }
-                }
+                    Modifier
+                        .border(
+                            Borders.BORDER_STANDARD,
+                            Colors.PRIMARY,
+                            Shapes.WINDOW
+                        )
+                        .padding(Spacers.NORMAL)
+                        .fillMaxSize(),
+                    Alignment.Center
+                ) { Picture() }
 
                 Row(Modifier.padding(Spacers.NORMAL)) {
-                    RotationButtons(mDVM)
+                    RotationButtons()
                     Spacer(Modifier.weight(1f))
-                    ZoomSpeedButton(mDVM)
+                    ZoomSpeedButton()
                 }
             }
         }
--- desktop/src/main/kotlin/com/github/picture2pc/desktop/viewmodel/mainscreen/PictureDisplayViewModel.kt
+++ desktop/src/main/kotlin/com/github/picture2pc/desktop/viewmodel/mainscreen/PictureDisplayViewModel.kt
@@ -8,6 +8,7 @@ import com.github.picture2pc.android.net.datatransmitter.DataTransmitter
 import com.github.picture2pc.common.net.data.payload.TcpPayload
 import com.github.picture2pc.desktop.data.RotationState
 import com.github.picture2pc.desktop.data.imageprep.PicturePreparation
+import com.github.picture2pc.desktop.data.next
 import com.github.picture2pc.desktop.extention.toImage
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -63,14 +64,12 @@ class PictureDisplayViewModel(
 
     fun rotate(clockwise: Boolean) {
         pP.rotate(clockwise)
+        mHVM.rotationState.value = mHVM.rotationState.value.next(clockwise)
     }
 
     fun reset() {
         mHVM.clear()
-        val picture = pictures.replayCache.getOrNull(selectedPictureIndex.value)
-        if (picture != null) {
-            setPicture(picture)
-        }
+        pictures.replayCache.getOrNull(selectedPictureIndex.value) ?: return
     }
 
     fun doAll() {
