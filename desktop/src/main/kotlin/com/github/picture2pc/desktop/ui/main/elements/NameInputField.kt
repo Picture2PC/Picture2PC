63ad42b473d364da7d468070e132dab7d3fc9dd6
GrumpyDucc <j.waldinger@online.de>
2024-11-27 19:04:14 +0100

    DESKTOP | moved preferences implementation to koin

--- desktop/src/main/kotlin/com/github/picture2pc/desktop/ui/main/elements/NameInputField.kt
+++ desktop/src/main/kotlin/com/github/picture2pc/desktop/ui/main/elements/NameInputField.kt
@@ -7,10 +7,8 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusManager
@@ -19,63 +17,42 @@ import androidx.compose.ui.input.key.key
 import androidx.compose.ui.input.key.onKeyEvent
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.unit.dp
-import com.github.picture2pc.common.data.preferences.PreferencesRepository
 import com.github.picture2pc.common.ui.Colors
 import com.github.picture2pc.common.ui.Heights
 import com.github.picture2pc.common.ui.Shapes
 import com.github.picture2pc.common.ui.TextStyles
 import com.github.picture2pc.desktop.ui.constants.Settings
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
+import com.github.picture2pc.desktop.viewmodel.mainscreen.ClientPreferencesViewModel
 import org.koin.compose.rememberKoinInject
-import org.koin.core.qualifier.named
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 @Composable
 fun NameInputField(
     focusManager: FocusManager,
-    preferencesRepository: PreferencesRepository = rememberKoinInject(),
-    coroutineScope: CoroutineScope = rememberKoinInject(named("viewModelCoroutineScope"))
+    viewModel: ClientPreferencesViewModel = rememberKoinInject()
 ) {
-    var name by remember { mutableStateOf(preferencesRepository.name.value) }
-    var isTextFieldError by remember { mutableStateOf(false) }
+    val name by viewModel.name.collectAsState()
+    val isTextFieldError by viewModel.isError.collectAsState()
 
     OutlinedTextField(
         value = name,
         onValueChange = {
-            if (it.length >= Settings.MAX_NAME_LENGTH) {
-                return@OutlinedTextField
-            } else {
-                isTextFieldError = false
-                name = it
-            }
-            if (it.isEmpty()) {
-                name = it
-                isTextFieldError = true
-            }
-            coroutineScope.launch {
-                preferencesRepository.setConnectable(!isTextFieldError)
-            }
+            viewModel.nameChanged(it)
         },
         placeholder = { Text("Username") },
         label = { Text("Name") },
         modifier = Modifier
             .fillMaxWidth()
             .height(Heights.BUTTON + 10.dp)
-
             .onKeyEvent { keyEvent ->
-                if (keyEvent.key == Key.Enter && !isTextFieldError) {
-                    coroutineScope.launch {
-                        name = name.trim()
-                        preferencesRepository.setName(name)
-                    }
+                if (!isTextFieldError && keyEvent.key == Key.Enter || keyEvent.key == Key.NumPadEnter) {
+                    viewModel.saveName(name)
                     focusManager.clearFocus()
                 }
                 true
             }
-
             .onGloballyPositioned {
-                isTextFieldError = name.length >= Settings.MAX_NAME_LENGTH || name.isEmpty()
+                viewModel.setError(name.length >= Settings.MAX_NAME_LENGTH || name.isEmpty())
             },
         singleLine = true,
         shape = Shapes.BUTTON,
