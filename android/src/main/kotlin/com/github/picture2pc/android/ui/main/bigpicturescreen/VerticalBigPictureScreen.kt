237becb97633f42f3f74690ce51d09daabd9b112
GrumpyDucc <grumpyduckmail@gmail.com>
2025-01-01 17:56:34 +0100

    ANDROID | added zooming into big picture

--- android/src/main/kotlin/com/github/picture2pc/android/ui/main/bigpicturescreen/VerticalBigPictureScreen.kt
+++ android/src/main/kotlin/com/github/picture2pc/android/ui/main/bigpicturescreen/VerticalBigPictureScreen.kt
@@ -3,6 +3,7 @@ package com.github.picture2pc.android.ui.main.bigpicturescreen
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,16 +15,26 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
 import com.github.picture2pc.android.ui.main.bigpicturescreen.elements.BottomOfScreen
 import com.github.picture2pc.android.viewmodel.camerascreenviewmodels.CameraViewModel
 import com.github.picture2pc.android.viewmodel.screenselectorviewmodels.ScreenSelectorViewModel
 import com.github.picture2pc.common.ui.Colors
+import kotlinx.coroutines.coroutineScope
 import org.koin.compose.rememberKoinInject
 
 @Composable
@@ -32,13 +43,30 @@ fun VerticalBigPictureScreen(
     screenSelectorViewModel: ScreenSelectorViewModel = rememberKoinInject()
 ) {
     val image = cameraViewModel.takenImage.collectAsState().value?.first
+    var scale by remember { mutableFloatStateOf(1f) }
+    var offset by remember { mutableStateOf(Offset.Zero) }
+    var lastScale by remember { mutableFloatStateOf(1f) }
+    var lastOffset by remember { mutableStateOf(Offset.Zero) }
 
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(20.dp)
+            .padding(20.dp).pointerInput(Unit) {
+                coroutineScope {
+                    detectTransformGestures { _, pan, zoom, _ ->
+                        scale = lastScale * zoom
+                        offset = lastOffset + pan
+                    }
+                }
+            }
     ) {
-        Row(modifier = Modifier.align(Alignment.Center)) {
+        Row(modifier = Modifier.align(Alignment.Center)
+            .graphicsLayer(
+                scaleX = scale,
+                scaleY = scale,
+                translationX = offset.x,
+                translationY = offset.y
+            )) {
             if (image != null)
                 Image(
                     image.asImageBitmap(),
@@ -62,6 +90,10 @@ fun VerticalBigPictureScreen(
             )
             BottomOfScreen()
         }
+    }
+    LaunchedEffect(scale, offset) {
+        lastScale = scale
+        lastOffset = offset
     }
     BackHandler {
         screenSelectorViewModel.toCamera()
