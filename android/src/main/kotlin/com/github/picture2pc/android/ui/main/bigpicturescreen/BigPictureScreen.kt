4f9e51ff942af2cb5504a48971b4422b889abdf7
GrumpyDucc <grumpyduckmail@gmail.com>
2025-03-19 08:26:01 +0100

    ANDROID | Updated color references to use common style constants

--- android/src/main/kotlin/com/github/picture2pc/android/ui/main/bigpicturescreen/BigPictureScreen.kt
+++ android/src/main/kotlin/com/github/picture2pc/android/ui/main/bigpicturescreen/BigPictureScreen.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
@@ -25,6 +24,7 @@ import androidx.compose.ui.unit.dp
 import com.github.picture2pc.android.ui.main.bigpicturescreen.elements.BottomOfScreen
 import com.github.picture2pc.android.viewmodel.camerascreenviewmodels.CameraViewModel
 import com.github.picture2pc.android.viewmodel.screenselectorviewmodels.ScreenSelectorViewModel
+import com.github.picture2pc.common.ui.Style
 import org.koin.compose.rememberKoinInject
 
 @Composable
@@ -63,7 +63,7 @@ fun BigPictureScreen(
                         .clip(CircleShape)
                         .padding(top = 10.dp, bottom = 10.dp),
                     thickness = 4.dp,
-                    color = MaterialTheme.colorScheme.primary
+                    color = Style.Colors.PRIMARY
                 )
             }
             BottomOfScreen()
