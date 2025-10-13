3477090abae7889301d3be168354e0823ad0c580
Maik <Maik_Julien.Rostocki@mail.schwarz>
2025-10-13 16:08:32 +0200

    COMMON | Changed multicast ip for better compatibility

--- common/src/main/kotlin/com/github/picture2pc/common/net/networkpayloadtransceiver/impl/multicast/MulticastConstants.kt
+++ common/src/main/kotlin/com/github/picture2pc/common/net/networkpayloadtransceiver/impl/multicast/MulticastConstants.kt
@@ -1,7 +1,7 @@
 package com.github.picture2pc.common.net.networkpayloadtransceiver.impl.multicast
 
 object MulticastConstants {
-    const val ADDRESS = "232.242.119.181"
+    const val ADDRESS = "224.0.0.69"
     const val PORT = 42851
     const val RETRY_DELAY = 2000L
     const val POLLING_TIMEOUT = 2000
