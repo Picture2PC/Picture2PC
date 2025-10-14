a040b1e52539b747b22ea985853ee5f77a153f63
mightytry <50262478+mightytry@users.noreply.github.com>
2025-10-14 22:24:10 +0200

    Fix/common/101 potential circular package issue when forwarding to both network adapters (#109)
    * COMMON | Changed multicast ip for better compatibility
    
    * COMMON | Remove relay function for now
    
    * COMMON | Remove comment
    
    ---------
    
    Co-authored-by: Maik <Maik_Julien.Rostocki@mail.schwarz>

--- common/src/main/kotlin/com/github/picture2pc/common/net/networkpayloadtransceiver/impl/multicast/MulticastPayloadTransceiver.kt
+++ common/src/main/kotlin/com/github/picture2pc/common/net/networkpayloadtransceiver/impl/multicast/MulticastPayloadTransceiver.kt
@@ -8,7 +8,6 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withTimeoutOrNull
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.get
 import java.net.NetworkInterface
@@ -60,11 +59,6 @@ class MulticastPayloadTransceiver(
                     return@launch
                 }
                 val payload = multicastSocket.receivePayload() ?: continue
-                launch {
-                    withTimeoutOrNull(2000) {
-                        sendPayloadExcluding(payload, multicastSocket)
-                    }
-                }
                 receivedPayload(payload)
             }
         }
