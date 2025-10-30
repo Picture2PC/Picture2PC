e18d8d29f32c7ab8edf91c8f829148ecbc139142
GrumpyDucc <grumpyduckmail@gmail.com>
2025-10-30 21:22:44 +0100

    theme implementation

---
+++ composeApp/src/commonMain/kotlin/org/picture2pc/picture2pc/presentation/theme/Type.kt
@@ -0,0 +1,46 @@
+package org.picture2pc.picture2pc.presentation.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontFamily
+
+import org.jetbrains.compose.resources.Font
+import picture2pc.composeapp.generated.resources.Inter
+import picture2pc.composeapp.generated.resources.Jost
+import picture2pc.composeapp.generated.resources.Res
+
+val bodyFontFamily
+    @Composable get() = FontFamily(
+        Font(
+            resource = Res.font.Jost
+        )
+    )
+
+val displayFontFamily
+    @Composable get() = FontFamily(
+        Font(
+            resource = Res.font.Inter
+        )
+    )
+
+val baseline = Typography()
+
+val Typography: Typography
+    @Composable get() = Typography(
+        displayLarge = baseline.displayLarge.copy(fontFamily = displayFontFamily),
+        displayMedium = baseline.displayMedium.copy(fontFamily = displayFontFamily),
+        displaySmall = baseline.displaySmall.copy(fontFamily = displayFontFamily),
+        headlineLarge = baseline.headlineLarge.copy(fontFamily = displayFontFamily),
+        headlineMedium = baseline.headlineMedium.copy(fontFamily = displayFontFamily),
+        headlineSmall = baseline.headlineSmall.copy(fontFamily = displayFontFamily),
+        titleLarge = baseline.titleLarge.copy(fontFamily = displayFontFamily),
+        titleMedium = baseline.titleMedium.copy(fontFamily = displayFontFamily),
+        titleSmall = baseline.titleSmall.copy(fontFamily = displayFontFamily),
+        bodyLarge = baseline.bodyLarge.copy(fontFamily = bodyFontFamily),
+        bodyMedium = baseline.bodyMedium.copy(fontFamily = bodyFontFamily),
+        bodySmall = baseline.bodySmall.copy(fontFamily = bodyFontFamily),
+        labelLarge = baseline.labelLarge.copy(fontFamily = bodyFontFamily),
+        labelMedium = baseline.labelMedium.copy(fontFamily = bodyFontFamily),
+        labelSmall = baseline.labelSmall.copy(fontFamily = bodyFontFamily),
+    )
+
